2745af1e1ea3689f02e2c6692a079f6cde9100e9
Elise Moura <88293904+lihmooura@users.noreply.github.com>
2021-10-11 23:46:22 -0300

    removing firebase url

--- src/base.js
+++ src/base.js
@@ -1,19 +1,11 @@
 import firebase from "firebase";
-// import { getStorage } from "firebase/storage";
 
 const App = {
-  apiKey: "AIzaSyDTy9nHvdoNz8dt7oVEyvI0n6YNGnUPd5E",
-  authDomain: "eduedu1-221f6.firebaseapp.com",
-  databaseURL: "https://eduedu1-221f6-default-rtdb.firebaseio.com",
-  projectId: "eduedu1-221f6",
-  storageBucket: "eduedu1-221f6.appspot.com",
-  messagingSenderId: "201362118305",
-  appId: "1:201362118305:web:7826b233d904958c6ec2e0",
-  measurementId: "G-QGHZ77T49S"
+// firebase 
 };
 
 const firebaseApp = firebase.initializeApp(App);
-// const storage = getStorage(firebaseApp);
+
 
 export default firebaseApp;
 
